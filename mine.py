7702c35532172be565934824b596532142224e0b
sbludwig <37007242@sfcollege.edu>
2024-08-27 14:52:28 -0400

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,9 +1,2 @@
-# Write a line of code that prints "Hello world!".
-
-
-def hello_world():
-  # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
-  
-  return variable_name
-
-
+msg = "Hello world!"
+print(msg)
